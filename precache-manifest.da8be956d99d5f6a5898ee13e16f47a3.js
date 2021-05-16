7570185e3c6b885ec827faa212895d1083c67c52
liaporto <moonkoala23@gmail.com>
2021-05-15 21:11:21 -0300

    Updates

---
+++ precache-manifest.da8be956d99d5f6a5898ee13e16f47a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c6f94ebfd9b01c2bba1df9f931a9f9",
+    "url": "/daily-pokemon/index.html"
+  },
+  {
+    "revision": "3f84a79840282104d451",
+    "url": "/daily-pokemon/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3f84a79840282104d451",
+    "url": "/daily-pokemon/static/js/2.ec24c1fd.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/daily-pokemon/static/js/2.ec24c1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c881777437259fabb85",
+    "url": "/daily-pokemon/static/js/main.96b2a43c.chunk.js"
+  },
+  {
+    "revision": "246f6a0c83da999dabd9",
+    "url": "/daily-pokemon/static/js/runtime-main.a8f751db.js"
+  },
+  {
+    "revision": "6b4ffa55fdd7e087c6990685887572b4",
+    "url": "/daily-pokemon/static/media/pokeball.6b4ffa55.svg"
+  }
+]);
